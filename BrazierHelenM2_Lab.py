3f9846f52e20f53ae2f97bee0e36b684ba8f5cac
hdbrazier <104693569+hdbrazier@users.noreply.github.com>
2025-03-28 21:43:01 -0400

    Added gpa limit

--- BrazierHelenM2_Lab.py
+++ BrazierHelenM2_Lab.py
@@ -15,10 +15,12 @@ last_name = input("Enter student's last name or enter 'ZZZ' to quit:") # variabl
 while last_name != QUIT: # enter loop
     first_name = input("Enter student's first name: ")  # variable to store the input of students first name
     gpa = float(input("Enter student's GPA: ")) # store the sutdent GPA in variable
-    if gpa >= 3.5: # check if GPA qualifies for Dean's List and print confirmation
-        print(f"{first_name} {last_name} has made the Dean's List")
-    elif gpa >= 3.25: # check if GPA qualifies for Honor Roll and print confirmation
-        print(f"{first_name} {last_name} has made the Honor Roll")
-    else: # print exception if student does not qualify for either
-        print(f"{first_name} {last_name} does not qualify for the Dean's List or Honor Roll")
+    if gpa < 0 or gpa > 4:
+        print("GPA must be between 0.0 and 4.0")
+    if gpa >= 3.5 and gpa <= 4: # check if GPA qualifies for Dean's List and print confirmation
+        print(f"{first_name.title()} {last_name.title()} has made the Dean's List")
+    elif gpa >= 3.25 and gpa <= 4: # check if GPA qualifies for Honor Roll and print confirmation
+        print(f"{first_name.title()} {last_name.title()} has made the Honor Roll")
+    elif gpa <3.25: # print exception if student does not qualify for either
+        print(f"{first_name.title()} {last_name.title()} does not qualify for the Dean's List or Honor Roll")
     last_name = input("\nEnter student's last name or enter 'ZZZ' to quit: ") # continue loop
